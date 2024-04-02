7d7c75634ce149db3f8ae2d6079b0df84f5140fc
ankit@3blocks.io+1-at-873834915608 <ankit@3blocks.io>
2024-04-02 12:24:48 +0530

    fix: all

--- src/common-infra/config/database.ts
+++ src/common-infra/config/database.ts
@@ -3,7 +3,17 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 import { ConnectionManager, getConnectionManager } from 'typeorm';
 import { Product } from '../../routes/product/entity/product.entity';
 import { User } from '../../routes/user/entity/user.entity';
-import { DBConfig } from './db-config';
+
+export const DBConfig = {
+    "POSTGRES_URL": "postgres://default:o3PGFCb6JsES@ep-sweet-pine-a4kfdpur-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+    "POSTGRES_PRISMA_URL": "postgres://default:o3PGFCb6JsES@ep-sweet-pine-a4kfdpur-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15",
+    "POSTGRES_URL_NO_SSL": "postgres://default:o3PGFCb6JsES@ep-sweet-pine-a4kfdpur-pooler.us-east-1.aws.neon.tech:5432/verceldb",
+    "POSTGRES_URL_NON_POOLING": "postgres://default:o3PGFCb6JsES@ep-sweet-pine-a4kfdpur.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+    "POSTGRES_USER": "default",
+    "POSTGRES_HOST": "ep-sweet-pine-a4kfdpur-pooler.us-east-1.aws.neon.tech",
+    "POSTGRES_PASSWORD": "o3PGFCb6JsES",
+    "POSTGRES_DATABASE": "verceldb"
+}
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
--- src/main.module.ts
+++ src/main.module.ts
@@ -2,7 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from 'nestjs-config';
 import path from 'path';
-import { TypeOrmConfigService } from './common-infra/config/db';
+import { TypeOrmConfigService } from './common-infra/config/database';
 import { AllModules } from './export-module';
 
 @Global()
